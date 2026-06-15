2b049729de1ca32d989a86d93266dd181dd39942
ymd65536 <ymd65536@gmail.com>
2026-06-15 13:26:09 +0900

    feat: MXC SDKを使用したサンドボックスの起動機能を追加

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const greeting = "Hello, TypeScript!";
-console.log(greeting);
+const mxc_sdk_1 = require("@microsoft/mxc-sdk");
+if (!(0, mxc_sdk_1.getPlatformSupport)().isSupported) {
+    throw new Error('MXC not available on this host');
+}
+const tools = (0, mxc_sdk_1.getAvailableToolsPolicy)(process.env);
+const temp = (0, mxc_sdk_1.getTemporaryFilesPolicy)();
+const config = (0, mxc_sdk_1.createConfigFromPolicy)({
+    version: '0.7.0-alpha',
+    filesystem: {
+        readonlyPaths: tools.readonlyPaths,
+        readwritePaths: temp.readwritePaths,
+    },
+    network: { allowOutbound: false },
+    timeoutMs: 30_000,
+});
+config.process.commandLine = 'python -c "print(\'hello from sandbox\')"';
+const child = (0, mxc_sdk_1.spawnSandboxFromConfig)(config, { usePty: false });
+child.stdout.on('data', (d) => process.stdout.write(d));
+child.on('close', (code) => console.log('exit:', code));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-const greeting: string = "Hello, TypeScript!";
-console.log(greeting);
+import {
+  spawnSandboxFromConfig, createConfigFromPolicy,
+  getAvailableToolsPolicy, getTemporaryFilesPolicy,
+  getPlatformSupport,
+} from '@microsoft/mxc-sdk';
+
+if (!getPlatformSupport().isSupported) {
+  throw new Error('MXC not available on this host');
+}
+
+const tools = getAvailableToolsPolicy(process.env);
+const temp  = getTemporaryFilesPolicy();
+
+const config = createConfigFromPolicy({
+  version: '0.7.0-alpha',
+  filesystem: {
+    readonlyPaths:  tools.readonlyPaths,
+    readwritePaths: temp.readwritePaths,
+  },
+  network: { allowOutbound: false },
+  timeoutMs: 30_000,
+});
+config.process!.commandLine = 'python3 -c "print(\'hello from sandbox\')"';
+
+const child = spawnSandboxFromConfig(config, { usePty: false });
+child.stdout!.on('data', (d) => process.stdout.write(d));
+child.on('close', (code) => console.log('exit:', code));
