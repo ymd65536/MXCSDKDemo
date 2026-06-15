8524db9ddf8eb5e979ce04777dbefc2febe8b0b7
ymd65536 <ymd65536@gmail.com>
2026-06-15 18:01:55 +0900

    feat: Pythonサンプルを追加し、サンドボックスのコマンドラインを更新

--- samples/python_hello_world/index.ts
+++ samples/python_hello_world/index.ts
@@ -19,17 +19,12 @@ const config = createConfigFromPolicy({
   version: '0.7.0-alpha',
   filesystem: {
     readonlyPaths: [userHome],
-    readwritePaths: [userHome],
   },
   network: { allowOutbound: false },
   timeoutMs: 30_000,
 });
-config.process!.commandLine = `date;echo "# sample file ${userHome}" > sample.log;date`;
+config.process!.commandLine = 'date;ls -la python;python3 python/hello_world.py;date';
 
 const child = spawnSandboxFromConfig(config, { usePty: false });
 child.stdout!.on('data', (d) => process.stdout.write(d));
 child.on('close', (code) => console.log('exit:', code));
-
-// readwritePaths を設定しない場合は /bin/sh: sample.log: Operation not permitted
-// deniedPaths を設定すると /bin/sh: sample.log: Operation not permitted になる
-// readwritePathsとdeniedPathsの両方を設定すると /bin/sh: sample.log: Operation not permitted になる
