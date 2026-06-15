34a9a0c1e9a9691e54a349d91e530aec69387c19
ymd65536 <ymd65536@gmail.com>
2026-06-15 14:15:30 +0900

    feat: Pythonスクリプトを追加し、サンドボックスのコマンドラインを更新

--- samples/first/index.ts
+++ samples/first/index.ts
@@ -4,8 +4,6 @@ import {
   getPlatformSupport,
 } from '@microsoft/mxc-sdk';
 
-import os from 'node:os';
-
 if (!getPlatformSupport().isSupported) {
   throw new Error('MXC not available on this host');
 }
@@ -13,17 +11,16 @@ if (!getPlatformSupport().isSupported) {
 const tools = getAvailableToolsPolicy(process.env);
 const temp  = getTemporaryFilesPolicy();
 
-const userHome = os.homedir();
-
 const config = createConfigFromPolicy({
   version: '0.7.0-alpha',
   filesystem: {
-    readonlyPaths: [userHome],
+    readonlyPaths:  tools.readonlyPaths,
+    readwritePaths: temp.readwritePaths,
   },
   network: { allowOutbound: false },
   timeoutMs: 30_000,
 });
-config.process!.commandLine = 'date;ls -la python;python3 python/hello_world.py;date';
+config.process!.commandLine = 'python3 --version;which python3;uname -m';
 
 const child = spawnSandboxFromConfig(config, { usePty: false });
 child.stdout!.on('data', (d) => process.stdout.write(d));
